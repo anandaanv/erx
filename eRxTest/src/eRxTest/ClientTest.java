381c74d058ecb66c196423e702333c110ace2e09
anandaanv <anand1711@gmail.com>
2016-04-06 16:12:19 +0530

    codegen additions

--- eRxTest/src/eRxTest/ClientTest.java
+++ eRxTest/src/eRxTest/ClientTest.java
@@ -1,5 +1,6 @@
 package eRxTest;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,10 +11,12 @@ import com.erx.beans.Medicine;
 import com.erx.cache.MedicineCache;
 import com.erx.client.ifc.SessionManager;
 import com.erx.client.impl.SessionManagerImpl;
+import com.erx.ex.UserNotFoundException;
 import com.erx.obj.Dose;
 import com.erx.obj.Patient;
 import com.erx.ops.Session;
 import com.erx.service.input.ERxCreateOrderOutput;
+import com.erx.user.login.LoginAction;
 
 public class ClientTest {
 
@@ -31,7 +34,9 @@ public class ClientTest {
 //		createPr("Kushal", "8983328793", "0000000003");
 	}
 
-	private void createPr(String name, String phoneNumber, String patientId) {
+	private void createPr(String name, String phoneNumber, String patientId) throws UserNotFoundException {
+		LoginAction login = new LoginAction();
+		login.login("anand1711", "password");
 		SessionManager mgr = new SessionManagerImpl(); 
 		Patient p = new Patient();
 		p.setId(patientId);
@@ -40,10 +45,12 @@ public class ClientTest {
 		p.setName(name);
 		p.setPhoneNumber(phoneNumber);
 		
-		Session session = mgr.createSession(p);
-		session.addDiagnosys(CacheInitializer.D1);
-		session.addDiagnosys(CacheInitializer.D2);
-		List<Medicine> meds = session.getPossibleMedicines();
+		List<String> dns = new ArrayList<>();
+		dns.add(CacheInitializer.D1.toString());
+		dns.add(CacheInitializer.D2.toString());
+		Session session = mgr.createSession(p,dns );
+		
+		List<Medicine> meds = mgr.getPossibleMedicines(session);
 		if (meds.isEmpty()) {
 			System.out.println("meds is empty..................................");
 		}
@@ -55,21 +62,54 @@ public class ClientTest {
 	}
 	
 	@Test
-	public void testAkshay() {
+	public void testAkshay() throws UserNotFoundException {
+		LoginAction login = new LoginAction();
+		login.login("anand1711", "password");
 		SessionManager mgr = new SessionManagerImpl(); 
 		Patient p = new Patient();
 		p.setId("0000000002");
 		p.setName("Akshay");
 		p.setPhoneNumber("9922996630");
-		Session session = mgr.createSession(p);
-		session.addDiagnosys(CacheInitializer.D1);
-		session.addDiagnosys(CacheInitializer.D2);
+
+		List<String> dns = new ArrayList<>();
+		dns.add(CacheInitializer.D1.toString());
+		dns.add(CacheInitializer.D2.toString());
+		
+		Session session = mgr.createSession(p, dns);
+
 		List<Medicine> meds = session.getPossibleMedicines();
 		if (meds.isEmpty()) {
 			System.out.println("meds is empty..................................");
 		}
 		session.addPrescription(meds.get(0), Collections.singleton(Dose.AFTER_BREAKFAST), 30);
 		session.addPrescription(meds.get(2), Collections.singleton(Dose.BEFORE_SLEEPING), 30);
+		ERxCreateOrderOutput op = (ERxCreateOrderOutput) mgr.prepareSession(session);
+		session.setPrescriptionId(op.getId());
+		session.setCertPassword("anandpass");
+		mgr.completeSession(session);
+	}
+	
+	@Test
+	public void testD3() throws UserNotFoundException {
+		LoginAction login = new LoginAction();
+		login.login("anand1711", "password");
+		SessionManager mgr = new SessionManagerImpl(); 
+		Patient p = new Patient();
+		p.setId("0000000002");
+		p.setName("Akshay");
+		p.setPhoneNumber("9922996630");
+		
+		List<String> dns = new ArrayList<>();
+		dns.add(CacheInitializer.D3.toString());
+		
+		Session session = mgr.createSession(p, dns);
+		
+		List<Medicine> meds = session.getPossibleMedicines();
+		if (meds.isEmpty()) {
+			System.out.println("meds is empty..................................");
+		}
+		session.addPrescription(meds.get(4), Collections.singleton(Dose.AFTER_BREAKFAST), 30);
+//		session.addPrescription(meds.get(2), Collections.singleton(Dose.BEFORE_SLEEPING), 30);
 		ERxCreateOrderOutput op = (ERxCreateOrderOutput) mgr.prepareSession(session);
 		session.setPrescriptionId(op.getId());
 		session.setCertPassword("anandpass");
--- eRxTest/src/erx/test/util/gen/CreateDoctor.java
+++ eRxTest/src/erx/test/util/gen/CreateDoctor.java
@@ -6,43 +6,68 @@ import java.io.IOException;
 
 import org.apache.pdfbox.io.IOUtils;
 
+import com.erx.ex.UserNotFoundException;
+import com.erx.obj.Certificate;
+import com.erx.obj.Doctor;
+import com.erx.obj.User;
 import com.erx.service.handler.ERxHelper;
+import com.erx.user.doctor.RegisterNewDoctor;
+import com.erx.user.doctor.RegisterUser;
 
-import eRxDB.Certificate;
-import eRxDB.Doctor;
+import eRxDB.Role;
 import eRxDB.persistence.PersistenceWrapper;
 
 public class CreateDoctor {
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		Doctor doc = new Doctor();
-		doc.setName("Arati Vaidya");
-		doc.setRegno("66555332");
-		doc.setQualification("MD");
-		String username = "arati";
-		doc.setUsername(username);
-		String password = "password";
+	public static void main(String[] args) throws FileNotFoundException, IOException, UserNotFoundException {
+//		createRoles();
 		
-		doc.setEncryptedPassword(ERxHelper.encrypt(password));
-		
-		Certificate cer = new Certificate();
-		cer.setKey(ERxHelper.encrypt(username));
-		cer.setSignImage(IOUtils.toByteArray(new FileInputStream("resources/certificate.png")));
-		cer.setValue(IOUtils.toByteArray(new FileInputStream("resources/keystore.pfx")));
-		
+		createDoctor();
+	}
+
+
+
+	private static void createRoles() {
+		Role doc = new Role();
+		doc.setRoleName("DOCTOR");
+		Role user = new Role();
+		user.setRoleName("USER");
+		Role pharma = new Role();
+		pharma.setRoleName("PHARMA");
 		
 		PersistenceWrapper.getEntitymanager().getTransaction().begin();
-		PersistenceWrapper.getEntitymanager().persist(doc);
-		PersistenceWrapper.getEntitymanager().persist(cer);
+		PersistenceWrapper.save(doc);
+		PersistenceWrapper.save(user);
+		PersistenceWrapper.save(pharma);
 		PersistenceWrapper.getEntitymanager().getTransaction().commit();
+		
 	}
-	
-	
-	
-	/**
-	 * @deprecated Use {@link ERxHelper#encrypt(String)} instead
-	 */
-	public static String encrypt(String passwordToHash){
-		return ERxHelper.encrypt(passwordToHash);
+
+
+
+	private static void createDoctor() throws IOException, FileNotFoundException, UserNotFoundException {
+		User user = new User();
+		user.setEmail("anand@dheemantech.com");
+		String password = "password";
+		user.setEncryptedPassword(ERxHelper.encrypt(password));
+		user.setName("Anand Vaidya");
+		user.setPhoneNumber("7722078812");
+		user.setUserId("anand1711");
+		
+		RegisterUser reg = new RegisterUser(user);
+		reg.processRegistration();
+		reg.completeRegistration();
+		 
+		
+		Doctor doc = new Doctor(0, "Anand Vaidya", "MD", "66555332", user);
+		Certificate cer = new Certificate();
+		cer.setUser(user);
+		cer.setSignImage(IOUtils.toByteArray(new FileInputStream("resources/certificate.png")));
+		cer.setCertificate(IOUtils.toByteArray(new FileInputStream("resources/keystore.pfx")));
+		
+		RegisterNewDoctor docReg = new RegisterNewDoctor(doc);
+		docReg.processRegistration();
+		
+		docReg.updateCertificate(cer);
 	}
 }
